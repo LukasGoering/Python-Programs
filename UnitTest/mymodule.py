ae9931c3352a2801798bcbd83ad00f1a06f854c9
LukasGoering <lukas.goering@gmx.de>
2025-06-05 19:26:14 +0200

    Added Folder containing a Unit Test example.

---
+++ UnitTest/mymodule.py
@@ -0,0 +1,16 @@
+""" This script defines a square and a double function """
+
+def square(number):
+    """ This function returns the square of a given number """
+
+    return number ** 2
+
+def double(number):
+    """ This function returns twice the value of a given number """
+
+    return number * 2
+
+def add(a,b):
+    """ This function returns the sum of the given numbers"""
+
+    return a + b
